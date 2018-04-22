3fbf289a266628123ce10a9d8c662ca619f64c1c
Logan0255 <luyanzhilogan@gmail.com>
2018-04-22 17:49:36 +1000

    1,finishing multi-page app environment configuring 2,optimizing common chunks for different pages by using SplitChunksPlugin.

---
+++ mock/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Yanzhi on 2018/4/18.
+ */
+// mock/config.js
+module.exports = {
+    '/api/index': {
+        local: '/index.json'
+    }
+}
+
---
+++ src/pages/about.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Yanzhi on 2018/4/22.
+ */
